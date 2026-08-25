d304cbbd0f79ece287051b30b388188526beabbc
Renan Viotto <renanviottosantos@gmail.com>
2026-08-24 21:30:04 -0300

    Lista de Execicio de ambientação

---
+++ Lista de Execicio/Exe01/Program.cs
@@ -0,0 +1,33 @@
+﻿//PASSO A PASSO DO EXERCICIO 06a
+//1 - Criar um vetor de valores numericos
+//2 - usar um loop de repetiçao
+// 3 - gerar valores aleatorios
+// 4- guardar no vetor
+
+int[] VetorNaoOrdenado = new int[100];
+int temp;
+Random random = new Random();
+for(int i = 0; i < VetorNaoOrdenado.Length; i++)
+{
+    VetorNaoOrdenado[i] = random.Next(1, 100);
+    Console.Write(VetorNaoOrdenado[i] + " ");
+}
+
+for(int i = 0; i < VetorNaoOrdenado.Length; i++)
+{
+    for(int j = 0; j < VetorNaoOrdenado.Length - i - 1; j++)
+    {
+        if(VetorNaoOrdenado[j] > VetorNaoOrdenado[j + 1])
+        {
+            temp = VetorNaoOrdenado[j];
+            VetorNaoOrdenado[j] = VetorNaoOrdenado[j+1];
+            VetorNaoOrdenado[j+1] = temp;
+        }
+    }
+}
+
+Console.WriteLine("\n Ordenado: \n");
+for(int i = 0; i < VetorNaoOrdenado.Length; i++)
+{
+    Console.Write(VetorNaoOrdenado[i] + " ");
+}
---
+++ Lista de Execicio/Exe06B/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] VetorNaoOrdenado = new int[100];
+Random random = new Random();
+for(int i = 0; i < VetorNaoOrdenado.Length; i++)
+{
+    VetorNaoOrdenado[i] = random.Next(1, 100);
+    Console.Write(VetorNaoOrdenado[i] + " ");
+}
+
+Array.Sort(vetorNaoOrdenado);
+
+foreach (int i in vetorNaoOrdenado)
+{
+  Console.WriteLine(i);
+}
